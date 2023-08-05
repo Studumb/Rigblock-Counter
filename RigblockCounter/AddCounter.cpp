4b8e436b64c71b5ce507b6c49e5914dff860af87
Studumb <112404846+Studumb@users.noreply.github.com>
2023-08-05 22:44:34 +0700

    Update AddCounter.cpp

--- RigblockCounter/AddCounter.cpp
+++ RigblockCounter/AddCounter.cpp
@@ -21,16 +21,18 @@ int AddCounter::Release()
 	return DefaultRefCounted::Release();
 }
 
-//Whenever an editor launchs it will add the counter
+
 bool AddCounter::HandleMessage(uint32_t messageID, void* message)
 {
+	//Whenever an editor is launched it will add the counter
 	if (Editor.IsActive()) {
 		//CounterUI is defined in AddCounter.h
+		//We will load the layout of the counter
 		CounterUI->LoadByName(u"rigblockCounter");
-		CounterUI->SetParentWindow(WindowManager.GetMainWindow()->FindWindowByID(0x330C1CA5));
 		//0x330C1CA5 is the controlID of the Complexity Meter
-		CounterUI->FindWindowByID(id("rigCounter"))->SetLocation(-110, 0);
+		CounterUI->SetParentWindow(WindowManager.GetMainWindow()->FindWindowByID(0x330C1CA5));
 		//After the counter has its parent window we will change the location of it
+		CounterUI->FindWindowByID(id("rigCounter"))->SetLocation(-110, 0);		
 	}
 	return true;
 }
