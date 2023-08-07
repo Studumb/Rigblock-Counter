cc5332b3bcc2b39d2f71109dc064380a136bf1db
Studumb <112404846+Studumb@users.noreply.github.com>
2023-08-07 21:17:07 +0700

    Update AddCounter.cpp

--- RigblockCounter/AddCounter.cpp
+++ RigblockCounter/AddCounter.cpp
@@ -30,6 +30,7 @@ bool AddCounter::HandleMessage(uint32_t messageID, void* message)
 		//We will load the layout of the counter
 		CounterUI->LoadByName(u"rigblockCounter");
 		//0x330C1CA5 is the controlID of the Complexity Meter
+		//The counter will automatically hide when its parent window hide
 		CounterUI->SetParentWindow(WindowManager.GetMainWindow()->FindWindowByID(0x330C1CA5));
 		//After the counter has its parent window we will change the location of it
 		CounterUI->FindWindowByID(id("rigCounter"))->SetLocation(-110, 0);		
